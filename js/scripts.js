891806e8b69a6c9d1c6f195361b44132d44303c6
Nick Sullivan <nicholassull@gmail.com>
2021-10-26 19:26:40 -0700

    correct spelling error in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function add(num1, num2) {
   return num1 + num2;
  }
-function substract(num1, num2) {
+function subtract(num1, num2) {
   return num1 - num2;
 }
 function multiply(num1, num2) {
@@ -14,4 +14,4 @@ function divide(num1, num2) {
  const num1 = parseInt(prompt("Enter a number:"));
  const num2 = parseInt(prompt("Enter another number:"));
  
- alert(add(num1, num2)); 
+ alert(divide(num1, num2)); 
