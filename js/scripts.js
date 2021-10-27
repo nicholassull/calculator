41327e0205c475e5719d726228a714b27598b336
Nick Sullivan <nicholassull@gmail.com>
2021-10-26 19:24:21 -0700

    add multiply function to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,10 @@ function add(num1, num2) {
 function substract(num1, num2) {
   return num1 - num2;
 }
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
  
  const num1 = parseInt(prompt("Enter a number:"));
  const num2 = parseInt(prompt("Enter another number:"));
