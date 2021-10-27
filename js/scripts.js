447bb11f6bffdf88ddf792b2433441dca771aa83
Nick Sullivan <nicholassull@gmail.com>
2021-10-26 19:25:08 -0700

    add divide function to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,13 @@ function add(num1, num2) {
 function substract(num1, num2) {
   return num1 - num2;
 }
-
 function multiply(num1, num2) {
   return num1 * num2;
 }
- 
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
  const num1 = parseInt(prompt("Enter a number:"));
  const num2 = parseInt(prompt("Enter another number:"));
  
